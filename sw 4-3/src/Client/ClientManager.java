ddea4f0c87883ff4772e32f3d03d3bf389e300d8
KimMinHyeok <37571051+iu7726@users.noreply.github.com>
2018-03-23 21:15:48 +0900

    Member 클래스 추가 String pw String birth String name Long time
    memberSend(new Member())
    Member클래스 전송

--- sw 4-3/src/Client/ClientManager.java
+++ sw 4-3/src/Client/ClientManager.java
@@ -2,40 +2,46 @@ package Client;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Map;
 
 public class ClientManager {
 	private String ip = "192.168.0.130";
 	private int port = 20000;
 	private InetAddress inet;
 	private Socket socket;
-	private String my;
-	
+	private Boolean my;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+
 	public ClientManager() {
 		try {
 			inet = InetAddress.getByName(ip);
-		} catch (UnknownHostException e) {
+			connect();
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("d");
 		}
 	}
-	
+
 	public void connect() {
 		try {
-			socket = new Socket(inet,port);
+			socket = new Socket(ip, port);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void send(String str) {
 		try {
-			ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 			out.writeObject(str);
 			out.flush();
 			Thread.sleep(1000);
@@ -45,10 +51,11 @@ public class ClientManager {
 		}
 	}
 	
-	public void receive() {
+	public void memberSend(Member member) {
 		try {
-			ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-			Object my = in.readObject();
+			out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			out.writeObject(member);
+			out.flush();
 			Thread.sleep(1000);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -56,8 +63,37 @@ public class ClientManager {
 		}
 	}
 	
-	public Object print() {
-		return my;
+	public void dateSend(Date time) {
+		try {
+			out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			out.writeObject(time);
+			out.flush();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Boolean receive() {
+		try {
+			in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+			my = in.readBoolean();
+			Thread.sleep(1000);
+			return my;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
+	public Long dateReceive() {
+		
+		return 0L;
+	}
+
+	public Boolean result() {
+		return my;
+	}
+
 }
---
+++ sw 4-3/src/Client/Member.java
@@ -0,0 +1,38 @@
+package Client;
+
+public class Member {
+	private String pw;
+	private String name;
+	private String birth;
+	private Long time;
+	public Member(String pw, String name, String birth) {
+		this.setPw(pw);
+		this.setName(name);
+		this.setBirth(birth);
+		this.setTime(0L);
+	} 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public Long getTime() {
+		return time;
+	}
+	public void setTime(Long time) {
+		this.time = time;
+	}
+}
