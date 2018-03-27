f156065037f92d672b5703788202937a305fa717
204-16 <204-16@DESKTOP-RKNIIMB>
2018-03-27 16:46:23 +0900

    헤더번경, 파일 매니져 추가

---
+++ sw 4-3/src/server/FileManager.java
@@ -0,0 +1,44 @@
+package server;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import db.Member;
+
+public class FileManager {
+	
+	public static Map<String, Member> map = new HashMap<>();
+	
+    //회원디비 저장
+    public static void saveDB(String id, Member m) {
+		map.put(id, m);	
+		try (ObjectOutputStream fout = new ObjectOutputStream(new FileOutputStream(new File("DBFile", "Member.txt")))){
+			fout.writeObject(map);
+	        fout.flush();
+		} catch (Exception e) {
+			System.out.println("실패...");
+		}
+	}
+    
+    //회원디비 가져오기
+	public static void readDB() {
+		try (ObjectInputStream fin = new ObjectInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(new File("DBFile", "Member.txt"))));){
+           map= (Map<String, Member>) fin.readObject();
+        } catch (Exception e) {
+            map = new HashMap<>();
+       }
+	}
+	
+	//아이디 중복 확인
+	public static boolean IDcheck(String id) {		
+		return !map.containsKey(id);	//아이디 존재 하면 false
+	}
+}
