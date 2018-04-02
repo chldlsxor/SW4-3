860c1383395e03157bc75f49895ebb5a1ab48689
KimMinHyeok <37571051+iu7726@users.noreply.github.com>
2018-04-02 15:36:03 +0900

    충돌 커밋

--- sw 4-3/src/sign/Client.java
+++ sw 4-3/src/sign/Client.java
@@ -3,7 +3,7 @@ package sign;
 import java.io.IOException;
 import java.net.InetAddress;
 
-import Client.ClientManager2;
+//import Client.ClientManager2;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
@@ -11,9 +11,9 @@ public class Client {
 		String num = ip.getHostAddress();
 		String numArr[] = num.split("\\.");
 		Wait wait = new Wait(numArr[3]);
-//		Login login = new Login(numArr[3]);
+		Login login = new Login(numArr[3]);
 		Charger charger = new Charger();
-//		JOptionPane.showInputDialog("입력");
+		JOptionPane.showInputDialog("입력");
 		ClientManager2 cm = new ClientManager2();
 	}
 }
